c3e32d36515411ef071d86d796c68661a079b63d
Greg Driza <gdriza@uarts.edu>
2017-09-09 22:52:55 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,31 @@
+/*
+
+**TODO**: In `arrays.js`, define a variable called `chocolateBars`.
+Its value should be an array of the strings `snickers`, `hundred grand`, `kitkat`, and `skittles`.
+
+
+**TODO**: In `arrays.js`, define two functions, `addElementToBeginningOfArray` and `
+destructivelyAddElementToBeginningOfArray`. Both functions take two parameters,
+an array and an element to add to the beginning of the array, and both functions
+should add the element to the beginning of the array and then return the whole
+array. The destructive function, `destructivelyAddElementToBeginningOfArray`,
+should alter the original array that's passed in; `addElementToBeginningOfArray`,
+on the other hand, should return a new array **and not modify the original**.
+
+**TODO**: Define two more functions, `addElementToEndOfArray` and
+`destructivelyAddElementToEndOfArray`. These functions also take two arguments,
+an array and an element to add to the end of the array. `addElementToEndOfArray`
+**should not** alter the original array; `destructivelyAddElementToEndOfArray`
+**should** alter the original array.
+
+
+**TODO**: Define a function in `arrays.js` called `accessElementInArray`.
+The function should accept an array and an index and return the element at that index.
+
+**TODO**: Define a function in `arrays.js` called `removeElementFromBeginningOfArray`
+that takes an array as its only argument and removes the first element.
+Your function should then return the entire array, and it **should not** mutate the underlying array.
+
+
+
+*/
