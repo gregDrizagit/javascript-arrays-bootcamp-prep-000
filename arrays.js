1dde71121bf40a0da6fe7b036a5387de1db4dd81
Greg Driza <gdriza@uarts.edu>
2017-09-09 23:40:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -53,9 +53,9 @@ var chocolateBars =[`snickers`, `hundred grand`, `kitkat`, `skittles`];
 
 function addElementToBeginningOfArray(array, element)
 {
-  var newArray = array.unshift(element);
+  //var newArray = ;
 
-  return newArray;
+  return array.unshift(element);
 
 }
 
