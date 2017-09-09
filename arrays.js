b0ebd4e74b80a78c9a23c060ec5292d542b26aa4
Greg Driza <gdriza@uarts.edu>
2017-09-09 23:48:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -62,9 +62,7 @@ function addElementToBeginningOfArray(array, element)
 function destructivelyAddElementToBeginningOfArray(array, element)
 {
 
-  array[0] = element;
-
-  return array;
+  return array.unshift(element);
 }
 
 function addElementToEndOfArray(array, element)
