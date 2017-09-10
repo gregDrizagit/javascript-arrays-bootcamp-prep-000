b90fd7e13fcdbb09b57ec75e62fca26088a50b83
Greg Driza <gdriza@uarts.edu>
2017-09-10 00:15:54 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -106,5 +106,5 @@ function destructivelyRemoveElementFromEndOfArray(array)
 
 function removeElementFromEndOfArray(array)
 {
-  return array.slice(array.length - 1);
+  return array.slice(array.length - 3);
 }
